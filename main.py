a67370ec98267d028e2df0ce18b28e399d5c68c6
Mauro Moureau <mauromoureau@icloud.com>
2025-02-05 10:13:38 +0100

    Log naar class

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import time
 import module.log as log
 
-log.create("flight.csv")
+
+try:
+    while True:
+
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("Logging stopped.")
--- module/log.py
+++ module/log.py
@@ -1,18 +1,18 @@
 # Mauro Moureau, 2025
 
 import os
-import time # seconden sinds 1 jan, 1970
+import time
 import csv
 
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 
-def create(log_name):
-    log_path = os.path.join(LOG_DIR, log_name)
+class Logger:
+    def __init__(self, log_name):
+        log_path = os.path.join(LOG_DIR, log_name)
 
-    if not os.path.exists(log_path):
-        with open(file=log_path, mode="w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(["time", "temperature", "pressure", "humidity", "gas"])
-    else:
-        print("\nVerwijder bestaand bestand met deze naam.\n")
+        if not os.path.exists(log_path):
+            with open(file=log_path, mode="w", newline="") as f:
+                writer = csv.writer(f)
+        else:
+            raise FileExistsError
